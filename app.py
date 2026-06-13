d7c392f62f0c76283b84b62e1b11502a3124f863
Adhitdmz <116830832+Adhitdmz@users.noreply.github.com>
2026-06-14 00:59:38 +0700

    Update app.py with prediction form

--- app.py
+++ app.py
@@ -1,5 +1,96 @@
 import streamlit as st
+import joblib
+import pandas as pd
 
-st.title("Telco Customer Churn Prediction")
+# Load model
+model = joblib.load("churn_model.pkl")
 
-st.write("Model Telco Customer Churn berhasil dimuat")
+st.title("📞 Telco Customer Churn Prediction")
+
+st.write("Masukkan data pelanggan untuk memprediksi churn.")
+
+gender = st.selectbox("Gender", ["Male", "Female"])
+SeniorCitizen = st.selectbox("Senior Citizen", [0, 1])
+Partner = st.selectbox("Partner", ["Yes", "No"])
+Dependents = st.selectbox("Dependents", ["Yes", "No"])
+
+tenure = st.number_input("Tenure (bulan)", 0, 100, 12)
+
+PhoneService = st.selectbox("Phone Service", ["Yes", "No"])
+MultipleLines = st.selectbox("Multiple Lines", ["Yes", "No", "No phone service"])
+
+InternetService = st.selectbox(
+    "Internet Service",
+    ["DSL", "Fiber optic", "No"]
+)
+
+OnlineSecurity = st.selectbox("Online Security", ["Yes", "No"])
+OnlineBackup = st.selectbox("Online Backup", ["Yes", "No"])
+DeviceProtection = st.selectbox("Device Protection", ["Yes", "No"])
+TechSupport = st.selectbox("Tech Support", ["Yes", "No"])
+StreamingTV = st.selectbox("Streaming TV", ["Yes", "No"])
+StreamingMovies = st.selectbox("Streaming Movies", ["Yes", "No"])
+
+Contract = st.selectbox(
+    "Contract",
+    ["Month-to-month", "One year", "Two year"]
+)
+
+PaperlessBilling = st.selectbox(
+    "Paperless Billing",
+    ["Yes", "No"]
+)
+
+PaymentMethod = st.selectbox(
+    "Payment Method",
+    [
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)"
+    ]
+)
+
+MonthlyCharges = st.number_input(
+    "Monthly Charges",
+    0.0,
+    200.0,
+    70.0
+)
+
+TotalCharges = st.number_input(
+    "Total Charges",
+    0.0,
+    10000.0,
+    1000.0
+)
+
+if st.button("Prediksi"):
+    data = pd.DataFrame({
+        'gender':[gender],
+        'SeniorCitizen':[SeniorCitizen],
+        'Partner':[Partner],
+        'Dependents':[Dependents],
+        'tenure':[tenure],
+        'PhoneService':[PhoneService],
+        'MultipleLines':[MultipleLines],
+        'InternetService':[InternetService],
+        'OnlineSecurity':[OnlineSecurity],
+        'OnlineBackup':[OnlineBackup],
+        'DeviceProtection':[DeviceProtection],
+        'TechSupport':[TechSupport],
+        'StreamingTV':[StreamingTV],
+        'StreamingMovies':[StreamingMovies],
+        'Contract':[Contract],
+        'PaperlessBilling':[PaperlessBilling],
+        'PaymentMethod':[PaymentMethod],
+        'MonthlyCharges':[MonthlyCharges],
+        'TotalCharges':[TotalCharges]
+    })
+
+    prediction = model.predict(data)
+
+    if prediction[0] == 1:
+        st.error("⚠️ Customer Diprediksi Churn")
+    else:
+        st.success("✅ Customer Diprediksi Tidak Churn")
